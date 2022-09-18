95a17d76fecf3b1871e63e65c69f22f542767564
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-18 12:58:49 +0500

    Import CSS file from ./style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./style/index.css";
 import Nav from "./components/Nav";
 import RouteSwitch from "./components/RouteSwitch";
 
