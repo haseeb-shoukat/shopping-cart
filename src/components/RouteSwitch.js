8c5b55c3fcc4a5d2aca3e6d0d4ab48f55cdd6e3d
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-18 14:12:58 +0500

    Add products prop and send it to Store component

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,12 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Store from "./Store";
+const products = [
+  { name: "Matt Black Cap, Unbranded, Unisex", price: 19.5, img: "cap" },
+  { name: "Nike Orange-White Jacket", price: 155, img: "jacket" },
+  { name: "Plain White T-Shirt for Men", price: 60, img: "tshirt" },
+  {
+    name: "Red Butterfly Dress + Black Belt for Women",
+    price: 245,
+    img: "dress",
+  },
+  {
+    name: "Rayban 2020 Limited Edition Shades",
+    price: 250,
+    img: "glasses",
+  },
+  {
+    name: "Stylish Denim Jeans for Women, Tight Fit",
+    price: 30,
+    img: "jeans",
+  },
+  { name: "Pink Polo Shirt, Slim Waist for Women", price: 20, img: "polo" },
+  { name: "Plain Black Shirt, Fitted, Men", price: 40, img: "shirt" },
+  { name: "Stylish High Heels, Full Black", price: 250, img: "shoes" },
+  { name: "Class Release, Two Piece Black Suit", price: 400, img: "suit" },
+];
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" />
+        <Route path="/products" element={<Store products={products} />} />
       </Routes>
     </BrowserRouter>
   );
