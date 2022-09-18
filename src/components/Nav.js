6cf1cbe46ce8c8987a721ba16506626d5dd8717f
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-18 14:12:34 +0500

    Add href for products tab

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,20 @@
 const Nav = () => {
   return (
     <div className="nav-bar">
-      <a className="nav-heading" href="#">
+      <a className="nav-heading" href="/">
         Knock-Offs
       </a>
       <nav>
-        <a className="nav-item active" href="#">
+        <a className="nav-item active" href="/">
           Home
         </a>
-        <a className="nav-item" href="#">
+        <a className="nav-item" href="/products">
           Products
         </a>
-        <a className="nav-item" href="#">
+        <a className="nav-item" href="/">
           Contact
         </a>
-        <a className="nav-item" href="#">
+        <a className="nav-item" href="/">
           <div className="circle-shape">
             <i className="fa fa-shopping-cart" aria-hidden="true"></i>
           </div>
