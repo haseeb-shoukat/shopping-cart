b96400c49fa4619028a600d2066a299feaaf088d
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-18 13:04:51 +0500

    Create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-card">
+        <div className="home-heading">BEST ONLINE STORE OF THE YEAR</div>
+        <div className="home-quote">
+          Style is a way to say who you are without having to speak
+        </div>
+      </div>
+      <img className="home-img" src="./home-cover.jpg" />
+    </div>
+  );
+};
+
+export default Home;
