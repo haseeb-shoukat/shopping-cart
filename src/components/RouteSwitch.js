2b2140d93ad7ce2b879da9f49f527fa9969e12a5
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-18 13:05:40 +0500

    Add Home path to Route Switch

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
         <Route path="/profile" />
       </Routes>
     </BrowserRouter>
