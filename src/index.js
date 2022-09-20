ace2fe8c99a6ccf65fe8876388b258fa66ea0aa4
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-20 20:45:02 +0500

    Move Nav render from index to RouteSwitch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
-import Nav from "./components/Nav";
 import RouteSwitch from "./components/RouteSwitch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Nav />
     <RouteSwitch />
   </React.StrictMode>
 );
