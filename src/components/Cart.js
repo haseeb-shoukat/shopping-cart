dbcf76a991da4fcbdd0c1896796dba33c1566631
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-21 12:05:41 +0500

    Create custom control knobs for incr/decr items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,27 +1,41 @@
-const Cart = ({ totalPrice, cartProducts }) => {
+const Cart = ({ hideCart, totalPrice, cartProducts }) => {
   return (
     <div className="overlay">
       <div className="cart">
         <div className="cart-heading">Your shopping cart</div>
         <div className="cart-products">
           {cartProducts.map((product, key) => {
-            <div key={key} className="cart-product">
-              <img
-                className="product-image-c"
-                src={`./images/clothing/${product.img}.png`}
-                alt=""
-              />
-              <div className="product-name">{product.name}</div>
-              <div className="product-price">
-                ${product.totalPrice.toFixed(2)}
+            return (
+              <div key={key} className="cart-product">
+                <img
+                  className="product-image-c"
+                  src={`./images/clothing/${product.img}.png`}
+                  alt=""
+                />
+                <div className="product-info-c">
+                  <div className="product-name no-wrap">{product.name}</div>
+                  <div className="product-price">
+                    ${product.totalPrice.toFixed(2)}
+                  </div>
+                  <div className="control-knobs">
+                    <button className="control-knob">
+                      <i class="fa fa-minus" aria-hidden="true"></i>
+                    </button>
+                    <div>{product.quantity}</div>
+                    <button className="control-knob">
+                      <i class="fa fa-plus" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                </div>
               </div>
-              <input type="number" step="1" />
-            </div>;
+            );
           })}
         </div>
         <div className="cart-total">Total price: ${totalPrice.toFixed(2)}</div>
         <button className="checkout-btn">Checkout</button>
-        <button className="close-btn">Close</button>
+        <button className="close-btn" onClick={hideCart}>
+          Close
+        </button>
       </div>
     </div>
   );
