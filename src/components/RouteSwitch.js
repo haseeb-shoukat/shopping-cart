7419b0c31e6af2fe68928870f6b64986b4a300af
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-09-21 12:42:35 +0500

    Use Product class instead of simple objects

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -5,69 +5,30 @@ import Store from "./Store";
 import Contact from "./Contact";
 import Nav from "./Nav";
 import Cart from "./Cart";
+import Product from "./Product";
 import { useState } from "react";
 
 const products = [
-  {
-    name: "Matt Black Cap, Unbranded, Unisex",
-    price: 19.5,
-    img: "cap",
-    key: uuidv4(),
-  },
-  {
-    name: "Nike Orange-White Jacket",
-    price: 155,
-    img: "jacket",
-    key: uuidv4(),
-  },
-  {
-    name: "Plain White T-Shirt for Men",
-    price: 60,
-    img: "tshirt",
-    key: uuidv4(),
-  },
-  {
-    name: "Red Butterfly Dress + Black Belt for Women",
-    price: 245,
-    img: "dress",
-    key: uuidv4(),
-  },
-  {
-    name: "Rayban 2020 Limited Edition Shades",
-    price: 250,
-    img: "glasses",
-    key: uuidv4(),
-  },
-  {
-    name: "Stylish Denim Jeans for Women, Tight Fit",
-    price: 30,
-    img: "jeans",
-    key: uuidv4(),
-  },
-  {
-    name: "Pink Polo Shirt, Slim Waist for Women",
-    price: 20,
-    img: "polo",
-    key: uuidv4(),
-  },
-  {
-    name: "Plain Black Shirt, Fitted, Men",
-    price: 40,
-    img: "shirt",
-    key: uuidv4(),
-  },
-  {
-    name: "Stylish High Heels, Full Black",
-    price: 250,
-    img: "shoes",
-    key: uuidv4(),
-  },
-  {
-    name: "Class Release, Two Piece Black Suit",
-    price: 400,
-    img: "suit",
-    key: uuidv4(),
-  },
+  new Product(uuidv4(), "Matt Black Cap, Unbranded, Unisex", 19.5, "cap"),
+  new Product(uuidv4(), "Nike Orange-White Jacket", 155, "jacket"),
+  new Product(uuidv4(), "Plain White T-Shirt for Men", 60, "tshirt"),
+  new Product(
+    uuidv4(),
+    "Red Butterfly Dress + Black Belt for Women",
+    245,
+    "dress"
+  ),
+  new Product(uuidv4(), "Rayban 2020 Limited Edition Shades", 250, "glasses"),
+  new Product(
+    uuidv4(),
+    "Stylish Denim Jeans for Women, Tight Fit",
+    30,
+    "jeans"
+  ),
+  new Product(uuidv4(), "Pink Polo Shirt, Slim Waist for Women", 20, "polo"),
+  new Product(uuidv4(), "Plain Black Shirt, Fitted, Men", 40, "shirt"),
+  new Product(uuidv4(), "Stylish High Heels, Full Black", 250, "shoes"),
+  new Product(uuidv4(), "Class Release, Two Piece Black Suit", 400, "suit"),
 ];
 
 const RouteSwitch = () => {
@@ -75,30 +36,6 @@ const RouteSwitch = () => {
 
   const showCart = () => setDisplayCart(true);
   const hideCart = () => setDisplayCart(false);
-
-  const cartProducts = [
-    {
-      name: "Matt Black Cap, Unbranded, Unisex",
-      price: 19.5,
-      img: "cap",
-      quantity: 8,
-      totalPrice: 56.098,
-    },
-    {
-      name: "Red Butterfly Dress + Black Belt for Women",
-      price: 245,
-      img: "dress",
-      quantity: 4,
-      totalPrice: 800,
-    },
-    {
-      name: "Plain White T-Shirt for Men",
-      price: 60,
-      img: "tshirt",
-      quantity: 4,
-      totalPrice: 700,
-    },
-  ];
 
   return (
     <BrowserRouter>
@@ -109,7 +46,7 @@ const RouteSwitch = () => {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       {displayCart ? (
-        <Cart hideCart={hideCart} totalPrice={0} cartProducts={cartProducts} />
+        <Cart hideCart={hideCart} totalPrice={0} cartProducts={products} />
       ) : null}
     </BrowserRouter>
   );
